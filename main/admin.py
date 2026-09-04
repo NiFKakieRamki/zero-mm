fe491fa656f6a4b1096c85f94bda3eb8bfb8fc85
yuliya <kiu268056@gmail.com>
2026-09-04 23:48:01 +0300

    Модели услуг, типов и зон, регистрация в админке

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProcedureType
+from .models import Zone
+from .models import Service
 
-# Register your models here.
+admin.site.register(ProcedureType)
+admin.site.register(Zone)
+admin.site.register(Service)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class ProcedureType(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Тип процедуры')
+
+    class Meta:
+        verbose_name = 'Тип процедуры'
+        verbose_name_plural = 'Типы процедур'
+
+    def __str__(self):
+        return self.name
+
+class Zone(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Зона')
+
+    class Meta:
+        verbose_name = 'Зона'
+        verbose_name_plural = 'Зоны'
+
+    def __str__(self):
+        return self.name
+
+class Service(models.Model):
+    procedure_type = models.ForeignKey('ProcedureType', on_delete=models.PROTECT, verbose_name='Тип процедуры')
+    zone = models.ForeignKey('Zone', on_delete=models.PROTECT, verbose_name='Зона')
+    duration_minutes = models.PositiveSmallIntegerField(verbose_name='Продолжительность процедуры')
+    price = models.DecimalField(max_digits=8, decimal_places=0, verbose_name='Цена')
+    is_active = models.BooleanField(default=True, verbose_name='Статус')
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+    def __str__(self):
+        return f'{self.procedure_type.name.upper()} - {self.zone} ( {self.duration_minutes} мин, {self.price} ₽ )'
